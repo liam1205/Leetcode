fae6ef03005e595cd7a98ef5b5dc054873f91fe8
Liam Greißing <liam.greissing@gmail.com>
2024-11-21 17:05:00 +0100

    add

--- src/Solution0019.java
+++ src/Solution0019.java
@@ -1,20 +1,3 @@
-class ListNode {
-
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
 class Solution0019 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int i = 0;
---
+++ src/Solution0141.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution0141 {
+
+    // not optimized
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        Map<ListNode, Integer> map = new HashMap<>();
+        while (head.next != null) {
+            if (map.containsKey(head.next)) {
+                return true;
+            } else {
+                map.put(head.next, 1);
+                head = head.next;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Solution0141 solver = new Solution0141();
+        ListNode first = new ListNode(3);
+        ListNode second = new ListNode(2);
+        first.next = second;
+        ListNode third = new ListNode(0);
+        second.next = third;
+        ListNode fourth = new ListNode(4);
+        fourth.next = second;
+        System.out.println(solver.hasCycle(first));
+    }
+}
